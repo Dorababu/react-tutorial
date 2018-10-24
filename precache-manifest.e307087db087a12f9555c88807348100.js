b1845ace187a8fda1529e4c653e9ad15d3ce0c47
Chodisetti Dorababu <dorababu.ch@gmail.com>
2018-10-24 15:18:48 +0530

    Updates

---
+++ precache-manifest.e307087db087a12f9555c88807348100.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5ee2de5f032faa4b14e3",
+    "url": "/static/js/main.5ee2de5f.chunk.js"
+  },
+  {
+    "revision": "83b44cb82af1de95abff",
+    "url": "/static/js/1.83b44cb8.chunk.js"
+  },
+  {
+    "revision": "5ee2de5f032faa4b14e3",
+    "url": "/static/css/main.8e4b60d3.chunk.css"
+  },
+  {
+    "revision": "b90fee64f8b12b0051b2fe5f08da126e",
+    "url": "/index.html"
+  }
+];
